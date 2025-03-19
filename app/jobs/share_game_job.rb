5142d82b1c608f0372572ef9ef657d8e278954f7
Florian Sitte <114937633+StarTyper@users.noreply.github.com>
2025-03-19 13:45:53 +0100

    logging share_game job

--- app/jobs/share_game_job.rb
+++ app/jobs/share_game_job.rb
@@ -2,6 +2,8 @@ class ShareGameJob < ApplicationJob
   queue_as :default
 
   def perform(user, recipient_email, share_code)
+    Rails.logger.info("ShareGameJob started for: #{recipient_email}")
     UserMailer.share_game(user, recipient_email, share_code).deliver_now
+    Rails.logger.info("ShareGameJob completed for: #{recipient_email}")
   end
 end
